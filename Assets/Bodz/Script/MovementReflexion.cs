af6a37b4aa06ead7473cf644b3697c66df9651d9
Garywiss <garcia_k@epitech.eu>
2014-11-17 16:40:38 +0100

    smap

---
+++ Assets/Bodz/Script/MovementReflexion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementReflexion : MonoBehaviour {
+
+    public GameObject RealPlayer;
+    public GameObject Border;
+
+	// Use this for initialization
+	void Start () 
+    {
+        transform.position = new Vector3(Border.transform.position.x - (RealPlayer.transform.position.x - Border.transform.position.x),
+            RealPlayer.transform.position.y,
+            RealPlayer.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        rigidbody2D.velocity = new Vector2(-RealPlayer.rigidbody2D.velocity.x, RealPlayer.rigidbody2D.velocity.y);
+	}
+}
