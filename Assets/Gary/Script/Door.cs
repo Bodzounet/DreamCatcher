07ff2009043bce6d8cf0748240ec6fc594119d51
Garywiss <garcia_k@epitech.eu>
2014-11-17 23:55:54 +0100

    ok

---
+++ Assets/Gary/Script/Door.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public int id;
+    public Transform other;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        if (c.gameObject.name == "CharacterLeft" && Input.GetAxis("Vertical") > 0 && !c.GetComponent<MovementController>().isEnter)
+        {
+            c.transform.position = other.position;
+            c.GetComponent<MovementController>().isEnter = true;
+        }
+    }
+}
