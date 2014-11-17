c482b1764237e5eef88eea858a698831a7d28575
boulot_j <boulot_j@epitech.eu>
2014-11-18 00:33:07 +0100

    tata

---
+++ Assets/Bodz/Script/MonsterFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterFactory : MonoBehaviour {
+
+    public GameObject model;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        GameObject monster;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            monster = Instantiate(model, transform.position, model.transform.rotation) as GameObject;
+	}
+}
---
+++ Assets/Bodz/Script/pathFinding.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pathFinding : MonoBehaviour {
+
+    public GameObject player;
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+	}
+}
