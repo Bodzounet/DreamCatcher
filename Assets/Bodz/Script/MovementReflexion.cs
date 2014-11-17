31e41625da92372db94411a3bc3714ca16679ec2
boulot_j <boulot_j@epitech.eu>
2014-11-17 18:04:19 +0100

    amélioration mouvement perso

--- Assets/Bodz/Script/MovementReflexion.cs
+++ Assets/Bodz/Script/MovementReflexion.cs
@@ -4,19 +4,35 @@ using System.Collections;
 public class MovementReflexion : MonoBehaviour {
 
     public GameObject RealPlayer;
-    public GameObject Border;
 
 	// Use this for initialization
 	void Start () 
     {
-        transform.position = new Vector3(Border.transform.position.x - (RealPlayer.transform.position.x - Border.transform.position.x),
-            RealPlayer.transform.position.y,
-            RealPlayer.transform.position.z);
+        setPos();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        rigidbody2D.velocity = new Vector2(-RealPlayer.rigidbody2D.velocity.x, RealPlayer.rigidbody2D.velocity.y);
+        setPos();
 	}
+
+    private void setPos()
+    {
+        float x = RealPlayer.transform.position.x;
+        float y = RealPlayer.transform.position.y;
+
+        //calc x pos of the mirrored player
+        RaycastHit2D tmp = Physics2D.Raycast(RealPlayer.transform.position, Vector3.right, Mathf.Infinity, 1 << 9);
+        if (tmp.transform != null)
+            x = RealPlayer.transform.position.x + 2 * Mathf.Abs(tmp.transform.position.x - x);
+
+        //calc y pos of the mirrored player
+        tmp = Physics2D.Raycast(RealPlayer.transform.position, Vector3.down, Mathf.Infinity, 1 << 9);
+        if (tmp.transform != null)
+            y = RealPlayer.transform.position.y - 2 * Mathf.Abs(tmp.transform.position.y - y);
+
+        transform.position = new Vector3(x, y, RealPlayer.transform.position.z);
+    }
+
 }
