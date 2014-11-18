877e866e8731bb0da6133ceb9f448d679a27257a
boulot_j <boulot_j@epitech.eu>
2014-11-18 10:06:58 +0100

    toto

---
+++ Assets/Bodz/Script/switchTorchMode.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchTorchMode : MonoBehaviour
+{
+
+    private bool isActive = false;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Fire")
+        {
+            isActive = true;
+            anim.SetBool("Active", true);
+        }
+        else if (col.tag == "Water")
+        {
+            isActive = false;
+            anim.SetBool("Active", false);
+        }
+    }
+}
