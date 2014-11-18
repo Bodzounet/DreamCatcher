e7c4da630976dfc0471a03d2a31d2d220832d14e
boulot_j <boulot_j@epitech.eu>
2014-11-18 12:08:18 +0100

    tata

--- Assets/Bodz/Script/pathFinding.cs
+++ Assets/Bodz/Script/pathFinding.cs
@@ -5,21 +5,34 @@ public class pathFinding : MonoBehaviour {
     
     public float speed = 1f;
     private Transform trans;
+
+    private float lastX;
+    private MovementController.e_dir lastDir = MovementController.e_dir.RIGHT;
+    private float scaleX;
     
     // Use this for initialization
     void Start () 
     {
         trans = GameObject.Find("CharacterRight").GetComponent<Transform>();
+        lastX = transform.position.x;
+        scaleX = transform.localScale.x;
     }
 	
     // Update is called once per frame
     void Update () 
     {
-        //Debug.Log("moving from : x : " + transform.position.x + " // y : " + transform.position.y);
-        //Debug.Log("moving to : x : " + player.transform.position.x + " // y : " + player.transform.position.y);
-
-        Debug.Log(trans.position);
+        MovementController.e_dir dir = MovementController.e_dir.NONE;
 
         transform.position = Vector2.MoveTowards(this.transform.position, trans.position, speed * Time.deltaTime);
+        if (trans.position.x < transform.position.x)
+            dir = MovementController.e_dir.LEFT;
+        else if (trans.position.x > transform.position.x)
+            dir = MovementController.e_dir.RIGHT;
+        lastX = transform.position.x;
+
+        if (dir == MovementController.e_dir.LEFT)
+            transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+        else
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
     }
 }
