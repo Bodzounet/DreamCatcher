990ae926bef3d3ea9685d34f3e25903a8ca1351d
SmoothyD <damien.blanc@epitech.eu>
2014-11-18 10:29:59 +0100

    dreameye

--- Assets/SmoothyD/scripts/CharacterInventory.cs
+++ Assets/SmoothyD/scripts/CharacterInventory.cs
@@ -9,9 +9,11 @@ public class CharacterInventory : MonoBehaviour {
     float                   visibleTime;
 	MicrophoneInput			microphoneInput;
 	double					timer;
+    double                  hiddenEntTimer;
 	string					blowChar;
 	SpriteRenderer			spriteRenderer;
     GameObject              hiddenEntities;
+    SpriteRenderer          dreamEye;
 	
 	// Use this for initialization
 	void Start () {
@@ -19,12 +21,15 @@ public class CharacterInventory : MonoBehaviour {
 		spriteRenderer = this.GetComponent<SpriteRenderer>();
 		blowChar = "BlowChar" + side;
 		timer = 0;
+        hiddenEntTimer = 0;
         if (side == "Left")
         {
             hiddenEntities = GameObject.Find("HiddenEntities");
             hiddenEntities.SetActive(false);
         }
         visibleTime = 3;
+        dreamEye = GameObject.Find("DreamEye").GetComponent<SpriteRenderer>();
+        dreamEye.color = new Color(1, 1, 1, 0);
 	}
 
     void HideEntities() {
@@ -33,12 +38,14 @@ public class CharacterInventory : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton ("BlowCharLeft") && Input.GetButton ("BlowCharRight") && microphoneInput.loudness > 15 && timer <= 0) {
+		if (Input.GetButton ("BlowCharLeft") && Input.GetButton ("BlowCharRight") && microphoneInput.loudness > 15 && hiddenEntTimer <= 0) {
             if (side == "Left")
             {
+                dreamEye.color = new Color(1, 1, 1, 0.75f);
                 hiddenEntities.SetActive(true);
                 Invoke("HideEntities", visibleTime);
                 visibleTime++;
+                hiddenEntTimer = visibleTime;
             }
 			timer = 0.75;
 		}
@@ -51,5 +58,9 @@ public class CharacterInventory : MonoBehaviour {
 		}
         if (timer > 0)
             timer -= Time.deltaTime;
+        if (hiddenEntTimer > 0) {
+            hiddenEntTimer -= Time.deltaTime;
+            dreamEye.color = new Color(1, 1, 1, (float)(hiddenEntTimer / visibleTime) * 0.8f);
+        }
 	}
 }
