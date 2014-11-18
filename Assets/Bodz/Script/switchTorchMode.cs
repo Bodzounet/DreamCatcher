560cf7011c6de74cbce22ce9654db0980531e86f
Garywiss <garcia_k@epitech.eu>
2014-11-18 15:46:21 +0100

    je t'aime damien

--- Assets/Bodz/Script/switchTorchMode.cs
+++ Assets/Bodz/Script/switchTorchMode.cs
@@ -4,32 +4,45 @@ using System.Collections;
 public class switchTorchMode : MonoBehaviour
 {
 
-    private bool isActive = false;
+    public bool isActive = false;
     public Animator anim;
-
+    public GameObject link;
+    public bool invert = false;
 	// Use this for initialization
 	void Start () 
     {
-	
+        anim = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-
+        if (link != null)
+        {
+                if (invert)
+                    link.SetActive(!isActive);
+                else
+                    link.SetActive(isActive);
+        }
+        anim.SetBool("isActive", isActive);
 	}
+
+    public void Invert()
+    {
+        isActive = true;
+        invert = true;
+        anim.SetBool("isActive", true);
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Fire")
         {
             isActive = true;
-            anim.SetBool("Active", true);
         }
         else if (col.tag == "Water")
         {
             isActive = false;
-            anim.SetBool("Active", false);
         }
     }
 }
