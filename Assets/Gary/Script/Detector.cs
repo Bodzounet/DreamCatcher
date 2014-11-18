337cb49e847ebcd582f7c89de7cf7e9104760610
Garywiss <garcia_k@epitech.eu>
2014-11-18 11:52:30 +0100

    LockedDoor

---
+++ Assets/Gary/Script/Detector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detector : MonoBehaviour {
+
+    public Key.KeyType type;
+	// Use this for initialization
+	void Start () 
+    {
+	        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        if (c.GetComponent<CharacterInventory>() != null && c.GetComponent<CharacterInventory>().key == type)
+        {
+            this.transform.parent.collider2D.enabled = false;
+            this.collider2D.enabled = false;
+        }
+    }
+}
