fd8f7044301a6280e86337ead7912af143396b1f
boulot_j <boulot_j@epitech.eu>
2014-11-19 12:06:33 +0100

    tata

--- Assets/Bodz/Menu/Script/micro.cs
+++ Assets/Bodz/Menu/Script/micro.cs
@@ -6,16 +6,21 @@ public class micro : MonoBehaviour {
     public TextMesh txt;
     public bool onOff = true;
 
+    public GameObject panel;
+    public Sprite[] spirtes;
+
     void OnMouseDown()
     {
         if (onOff)
         {
+            panel.GetComponent<SpriteRenderer>().sprite = spirtes[0];
             onOff = false;
             txt.text = "Off";
             PlayerPrefs.SetInt("micro", 0);
         }
         else
         {
+            panel.GetComponent<SpriteRenderer>().sprite = spirtes[1];
             onOff = true;
             txt.text = "On";
             PlayerPrefs.SetInt("micro", 1);
