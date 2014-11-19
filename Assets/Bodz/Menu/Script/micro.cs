9e1660a1f9ce2d46ef047b11150c47d147d5cc6c
Garywiss <garcia_k@epitech.eu>
2014-11-19 13:45:14 +0100

    ok

--- Assets/Bodz/Menu/Script/micro.cs
+++ Assets/Bodz/Menu/Script/micro.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+
+using UnityEngine;
 using System.Collections;
 
 public class micro : MonoBehaviour {
@@ -28,34 +28,4 @@ public class micro : MonoBehaviour {
         }
     }
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
 
-public class micro : MonoBehaviour {
-
-    public TextMesh txt;
-    public bool onOff = true;
-
-    public GameObject panel;
-    public Sprite[] spirtes;
-
-    void OnMouseDown()
-    {
-        if (onOff)
-        {
-            panel.GetComponent<SpriteRenderer>().sprite = spirtes[0];
-            onOff = false;
-            txt.text = "Off";
-            PlayerPrefs.SetInt("micro", 0);
-        }
-        else
-        {
-            panel.GetComponent<SpriteRenderer>().sprite = spirtes[1];
-            onOff = true;
-            txt.text = "On";
-            PlayerPrefs.SetInt("micro", 1);
-        }
-    }
-}
->>>>>>> 4c7c6adade5e85372af206f0fd9cd710083d96eb
--- Assets/Bodz/Menu/Script/returnMenu.cs
+++ Assets/Bodz/Menu/Script/returnMenu.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class returnMenu : MonoBehaviour {
@@ -11,17 +10,4 @@ public class returnMenu : MonoBehaviour {
         go.SetActive(false);
     }
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
 
-public class returnMenu : MonoBehaviour {
-
-    public GameObject go;
-
-    void OnMouseDown()
-    {
-        go.SetActive(false);
-    }
-}
->>>>>>> 4c7c6adade5e85372af206f0fd9cd710083d96eb
